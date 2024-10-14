b5f44520c43f71503fdbbf9b812f14c05e4db71e
Kory <koriellopez03@gmail.com>
2024-10-13 22:25:05 -0500

    collapsing info

--- src/components/CurrentSemester/CurrentSemester.js
+++ src/components/CurrentSemester/CurrentSemester.js
@@ -1,11 +1,28 @@
+import Chevron from "../../resources/icons/Chevron";
+
 function CurrentSemester() {
 	let getCourses = () => {
 		let courses = [];
 		for (let i = 0; i < 3; i++) {
 			courses.push(
-				<div className="card-text row py-4">
-					<div className="col col-8">Algorithms & Data Structures {i}</div>
-					<div className="col col-4">100%</div>
+				<div className="w-100 py-2">
+					<button
+						className="btn w-100"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target={`#collapse${i}`}
+					>
+						<div key={i} className="card-text row">
+							<div className="col col-1">
+								<Chevron />
+							</div>
+							<div className="col col-7">Algorithms & Data Structures {i}</div>
+							<div className="col col-4">100%</div>
+						</div>
+					</button>
+					<div className="collapse" id={`collapse${i}`}>
+						<p>something hidden here!</p>
+					</div>
 				</div>
 			);
 		}
@@ -14,10 +31,12 @@ function CurrentSemester() {
 
 	return (
 		<div className="row">
-			<div className="col col-8 ">
+			<div className="col col-8">
 				<div className="card bg-secondary-subtle">
 					<div className="card-body">
-						<div className="card-title h3">Courses</div>
+						<div className="card-title h3 border-bottom border-secondary">
+							Courses
+						</div>
 						<div className="card-subtitle h5">
 							<div className="row">
 								<div className="col col-8">Name</div>
