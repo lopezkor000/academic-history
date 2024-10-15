c084329bb90ad6953dd01840eb9e32d28bc19657
Kory <74568018+lopezkor000@users.noreply.github.com>
2024-10-14 23:13:58 -0500

    added name, details, removed old db

--- src/components/CurrentSemester/Courses/CourseTab.js
+++ src/components/CurrentSemester/Courses/CourseTab.js
@@ -1,34 +1,54 @@
 import Chevron from "../../../resources/icons/Chevron";
 import { useState } from "react";
 
-function CourseTab(props) {
-	let [open, setOpen] = useState(true);
+function CourseDetails(props) {
+  let makeDetails = () => {
+    let details = [];
+    for (let [key, value] of Object.entries(props.details))
+      details.push(
+        <p key={key}>
+          {key}: {String(value)}
+        </p>
+      );
+    return details;
+  };
 
-	return (
-		<div className="py-2">
-			<button
-				className="btn w-100"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target={`#${props.name}`}
-				onClick={() => {
-					setOpen(!open);
-					console.log(open);
-				}}
-			>
-				<div className="card-text row">
-					<div className="col col-1">
-						<Chevron open={open} />
-					</div>
-					<div className="col col-7">{props.name}</div>
-					<div className="col col-4">{props.details.grade}%</div>
-				</div>
-			</button>
-			<div className="collapse text-start ps-5 ms-5" id={`${props.name}`}>
-				<p>{props.details.name}</p>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="collapse text-start ps-5 border border-secondary"
+      id={`${props.name}`}
+    >
+      {makeDetails()}
+    </div>
+  );
+}
+
+function CourseTab(props) {
+  let [open, setOpen] = useState(true);
+
+  return (
+    <div className="py-2">
+      <button
+        className="btn w-100"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target={`#${props.name}`}
+        onClick={async () => {
+          setOpen(!open);
+        }}
+      >
+        <div className="card-text row">
+          <div className="col col-1">
+            <Chevron open={open} />
+          </div>
+          <div className="col col-2">{props.name}</div>
+          <div className="col col-7">{props.details.name}</div>
+          <div className="col col-2">{props.details.grade}%</div>
+        </div>
+        <CourseDetails name={props.name} details={props.details} />
+      </button>
+    </div>
+  );
 }
 
 export default CourseTab;
--- src/components/CurrentSemester/CurrentSemester.js
+++ src/components/CurrentSemester/CurrentSemester.js
@@ -3,45 +3,39 @@ import axios from "axios";
 import CourseTab from "./Courses/CourseTab";
 
 function CurrentSemester() {
-	let [courses, setCourses] = useState({});
+  let [courses, setCourses] = useState({});
 
-	useEffect(() => {
-		(async () => {
-			let result = await axios("http://localhost:8000/0");
-			let semester = result.data.years["2024"].semesters["fall"];
-			setCourses(semester.courses);
-		})();
-	}, []);
+  useEffect(() => {
+    (async () => {
+      let result = await axios("http://localhost:8000/0");
+      let semester = result.data.years["2024"].semesters["fall"];
+      setCourses(semester.courses);
+    })();
+  }, []);
 
-	let getCourses = () => {
-		let coursesDiv = [];
-		for (let [name, details] of Object.entries(courses)) {
-			coursesDiv.push(<CourseTab key={name} name={name} details={details} />);
-		}
-		return coursesDiv;
-	};
+  let getCourses = () => {
+    let coursesDiv = [];
+    for (let [name, details] of Object.entries(courses)) {
+      coursesDiv.push(<CourseTab key={name} name={name} details={details} />);
+    }
+    return coursesDiv;
+  };
 
-	return (
-		<div className="row">
-			<div className="col col-8">
-				<div className="card bg-secondary-subtle">
-					<div className="card-body">
-						<div className="card-title h3 border-bottom border-secondary">
-							Courses
-						</div>
-						<div className="card-subtitle h5">
-							<div className="row">
-								<div className="col col-8">Name</div>
-								<div className="col col-4">Grade</div>
-							</div>
-						</div>
-						{getCourses()}
-					</div>
-				</div>
-			</div>
-			<div className="col col-4 bg-primary">Summary</div>
-		</div>
-	);
+  return (
+    <div className="row">
+      <div className="col col-8">
+        <div className="card bg-secondary-subtle">
+          <div className="card-body">
+            <div className="card-title h3 border-bottom border-secondary">
+              Courses
+            </div>
+            {getCourses()}
+          </div>
+        </div>
+      </div>
+      <div className="col col-4 bg-primary">Summary</div>
+    </div>
+  );
 }
 
 export default CurrentSemester;
